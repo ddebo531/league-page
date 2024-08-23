921f443197afb7fbece37eb658f4f392ea46dd25
dom <15651999+ddebo531@users.noreply.github.com>
2024-08-23 09:52:44 -0400

    more update

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -150,8 +150,11 @@ export const managers = [
       "name": "dDebo531",
       "bio": "Lorem ipsum...",
       "favoriteTeam": "ne",
+      "favoritePlayer": 9229,
       "photo": "/managers/ddebo531.jpg", // square ratio recommended (no larger than 500x500)
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "rookieOrVets": "Rookies",
+      "valuePosition": "RB",
       "rival": {
         name: "Rival", // Can be anything (usually your rival's name)
         link: 1, // manager array number within this array, or null to link back to all managers page
